18def05782d95766d2c4d2d49001a40ebd7840f2
Kevin Osborn <kosborn2@ucsc.edu>
2017-01-06 17:21:08 -0800

    try mocing the linkage for the protoc module

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,15 @@ ga4gh.__path__.insert(0, '../ga4gh')
 
 import ga4gh.server
 
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['protoc']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
